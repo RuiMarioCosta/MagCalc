fc9d92039ec03b2ee626f54dd0825530164a6cb6
Rui Costa <ctw00533@criticaltechworks.com>
2020-12-05 22:25:50 +0000

    clean up and refactor code

--- __init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Feb 06 11:36:35 2017
-
-@author: Rui
-"""
-
--- gui_mce/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Feb 06 15:05:38 2017
-
-@author: Rui
-"""
-
-#import gui_mce
